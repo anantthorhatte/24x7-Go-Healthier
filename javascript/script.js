ead5a127baaec1ceefc33f190ad847a397b0a04d
12345Vaishnavi <vaishnavib259@gmail.com>
2024-04-11 00:23:37 +0530

    JavaScript code added

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+
+function scheduleAppointment(){
+  const takeAppointment = document.getElementById("appointmentDate").value;
+  console.log(takeAppointment);
+
+  const currentDate = new Date();
+ 
+ /* if()
+  {
+   document.write(` Appointment date scheduled successfully `);
+  }
+  else
+  {
+    document.write(` Appointment date hasn't scheduled successfully `);
+  } 
+  */
+}
