a0d937d4fa415c8c596c314d4960ef902c9ebb41
12345Vaishnavi <vaishnavib259@gmail.com>
2024-04-12 22:19:39 +0530

    Javascript code added

--- javascript/appointment.js
+++ javascript/appointment.js
@@ -1,19 +1,13 @@
 
-function scheduleAppointment(){
-  const takeAppointment = document.getElementById("appointmentDate");
-  console.log(`Oppointment Schedule on ${takeAppointment} date`);
-
-  const currentDate = new Date();
  
- /* if()
-  {
-   document.write(` Appointment date scheduled successfully `);
-  }
-  else
-  {
-    document.write(` Appointment date hasn't scheduled successfully `);
-  } 
-  */
-}
+ 
+function scheduleAppointment(){
+  let  confirmAppointment = confirm('Would you like to Schedule Appointment?');
+ alert(confirmAppointment);
 
-scheduleAppointment();
+ 
+ }
+
+
+
+ 
