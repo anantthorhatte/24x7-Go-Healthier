db48b3c452289bdf50880b559b85770d34b102e4
anantthorhatte <anantthorhatte@gmail.com>
2024-04-14 13:17:49 +0530

    javascript added in login and signup page

--- javascript/login.js
+++ javascript/login.js
@@ -1,7 +1,7 @@
-function login(){
-    let  confirmAppointment = confirm('Login Successfull!!!');
-   alert(confirmAppointment);
-  
-   
-   }
-  
+function showAlert() {
+    // Get the value of the input field
+    var searchTerm = document.getElementById("loginnInput").value;
+    
+    // Display an alert with the search term
+    alert("Welcome " + searchTerm );
+}
